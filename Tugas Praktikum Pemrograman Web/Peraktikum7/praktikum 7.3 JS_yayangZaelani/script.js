c4c3e8d905dd45a265fb79087fd08d3e6198527e
YayangZaelani <93021092+YayangZaelani@users.noreply.github.com>
2022-01-01 07:19:41 +0700

    Add files via upload

---
+++ Tugas Praktikum Pemrograman Web/Peraktikum7/praktikum 7.3 JS_yayangZaelani/script.js
@@ -0,0 +1,13 @@
+console.log('Hai, Zey!')
+
+// Belajar Variabel
+// statis
+const phi = 3.14;
+// phi = 3.15; error tidak bisa dikasih nilai ulang 
+console.log (phi)
+
+
+// dinamis
+let age = 19;
+age = 20;
+console.log (age)
